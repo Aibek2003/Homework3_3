e4cc4f91dc828d56c2398b97686cbb19a88fd0b1
Aibek2003 <raiymbekovaibek03@gmail.com>
2024-03-21 02:11:33 +0600

    новая ссылка

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BankAccount {
+class BankAccount {
+    private double amount;
+
+    public BankAccount(double initialAmount) {
+        this.amount = initialAmount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        amount += sum;
+    }
+
+    public void withDraw(double sum) throws LimitException {
+        if (sum > amount) {
+            throw new LimitException("Requested amount exceeds the balance", amount);
+        }
+        amount -= sum;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = new BankAccount(15000);
+
+        try {
+            while (true) {
+                try {
+                    account.withDraw(6000);
+                    System.out.println("Снято 6000 сом.Оставшаяся сумма: " + account.getAmount() + " сом.");
+                } catch (LimitException e) {
+                    System.out.println("Попытка вывести сумму, превышающую остаток.");
+                    System.out.println("Оставшаяся сумма на счету: " + e.getRemainingAmount() + " сом.");
+                    account.withDraw(e.getRemainingAmount()); // Снимаем только доступную сумму
+                    break; // Завершаем бесконечный цикл
+                }
+            }
+        } catch (LimitException e) {
+            System.out.println("Unexpected LimitException occurred: " + e.getMessage());
+        }
     }
 }
